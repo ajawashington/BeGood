0ffb7e08968322f2bd146334cd81633dcb1a08b2
Aja Washington <aja.washington13@gmail.com>
2021-02-16 12:33:27 -0600

    Updates

---
+++ precache-manifest.3c0fe0f1900e974c2eeaa521722dc334.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b865d402a8990596d225c6f2a13af05",
+    "url": "/beygood/index.html"
+  },
+  {
+    "revision": "e12dcef27b52b5fbd90a",
+    "url": "/beygood/static/css/main.4541bdd0.chunk.css"
+  },
+  {
+    "revision": "e0c9ced534e711283d8a",
+    "url": "/beygood/static/js/2.60d0809b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/beygood/static/js/2.60d0809b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e12dcef27b52b5fbd90a",
+    "url": "/beygood/static/js/main.d63c90dd.chunk.js"
+  },
+  {
+    "revision": "db2a860eb769da515d4e",
+    "url": "/beygood/static/js/runtime-main.141a14ba.js"
+  },
+  {
+    "revision": "bdd21f99d27101db1d4ea011d083a89b",
+    "url": "/beygood/static/media/boy.bdd21f99.gif"
+  },
+  {
+    "revision": "9ec306103519b2107fa4e4fdeff728c8",
+    "url": "/beygood/static/media/formation.9ec30610.jpg"
+  },
+  {
+    "revision": "44a0b38ed9d1af8a519d4fab67b8b1e9",
+    "url": "/beygood/static/media/logo.44a0b38e.jpg"
+  }
+]);
