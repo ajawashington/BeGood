5139d3184aa5938f1beb5e03b20f786428c71023
Aja Washington <57641436+ajawashington@users.noreply.github.com>
2021-03-02 09:41:27 -0600

    Update BusinessProvider.js

--- src/components/businesses/BusinessProvider.js
+++ src/components/businesses/BusinessProvider.js
@@ -1,14 +1,7 @@
 import React, { useState, useEffect } from "react"
 
-/*
-    The context is imported and used by individual components
-    that need data
-*/
 export const BusinessContext = React.createContext()
 
-/*
- This component establishes what data can be used.
- */
 export const BusinessProvider = (props) => {
     const [businesses, setBusinesses] = useState([])
 
@@ -39,17 +32,11 @@ export const BusinessProvider = (props) => {
         })
             .then(getBusinesses)
     }
-
-    /*
-        Load all animals when the component is mounted. Ensure that
-        an empty array is the second argument to avoid infinite loop.
-    */
     useEffect(() => {
         getBusinesses()
     }, [])
 
     useEffect(() => {
-        console.log("****  Business APPLICATION STATE CHANGED  ****")
     }, [businesses])
 
     return (
