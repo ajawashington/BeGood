6b2a96cf1a2bcb98a82f9fea5e3af136926834a6
ajawashington <ajawashington13@gmail.com>
2021-03-03 10:00:13 -0600

    zsh

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,31 +1,43 @@
-import React from "react"
-import { Route } from "react-router-dom"
-import CharityRequestList from "./charity/CharityRequestList"
-import CharityRequestForm from "./charity/CharityRequestForm"
-import DonationList from "./donations/DonationList"
-import ProviderProvider from "./ProviderProvider"
+import React from "react";
+import { Route } from "react-router-dom";
+import CharityRequestList from "./charity/CharityRequestList";
+import CharityRequestForm from "./charity/CharityRequestForm";
+import DonationList from "./donations/DonationList";
+import ProviderProvider from "./ProviderProvider";
 // import DashboardList from "./dashboard/DashboardList"
-import BusinessList from "./businesses/BusinessList"
-import BusinessDetails from "./businesses/BusinessDetails"
+import BusinessList from "./businesses/BusinessList";
+import BusinessDetails from "./businesses/BusinessDetails";
 
-
-export default props => {
-    return (
-        <>
-     <ProviderProvider>
-     <Route exact path="/business" render={props => <BusinessList {...props} />} />
-
-     <Route exact path="/businesses/:businessId(\d+)" render={props => <BusinessDetails {...props} />} />
-        <Route exact path="/" render={props => <CharityRequestForm {...props} />} />
-        <Route exact path="/" render={props => <DonationList {...props} />} />
-        <Route exact path="/donor" render={props => <CharityRequestList {...props} />} />
-        <Route path="/:charityRequestId(\d+)"render={props => <CharityRequestForm {...props} />} />
-
-
-        {/* <Route path="/edit/:charityRequestId(\d+)"render={props => <CharityRequestForm {...props} />} /> */}
-
-</ProviderProvider>
-
-</>
-)
-}
+export default (props) => {
+  return (
+    <>
+      <ProviderProvider>
+        <Route
+          exact
+          path="/business"
+          render={(props) => <BusinessList {...props} />}
+        />
+        <Route
+          exact
+          path="/businesses/:businessId(\d+)"
+          render={(props) => <BusinessDetails {...props} />}
+        />
+        <Route
+          exact
+          path="/"
+          render={(props) => <CharityRequestForm {...props} />}
+        />
+        <Route exact path="/" render={(props) => <DonationList {...props} />} />
+        <Route
+          exact
+          path="/donor"
+          render={(props) => <CharityRequestList {...props} />}
+        />
+        <Route
+          path="/:charityRequestId(\d+)"
+          render={(props) => <CharityRequestForm {...props} />}
+        />
+      </ProviderProvider>
+    </>
+  );
+};
--- src/components/ProviderProvider.js
+++ src/components/ProviderProvider.js
@@ -1,24 +1,24 @@
-import React from "react"
-import { CharityRequestProvider } from "./charity/CharityRequestProvider"
-import { BusinessProvider } from "./businesses/BusinessProvider"
-import { DonationProvider } from "./donations/DonationProvider"
-import { BusinessTypeProvider } from "./businesses/BusinessTypeProvider"
-import { DonorProvider } from "./donations/DonorProvider"
+import React from "react";
+import { CharityRequestProvider } from "./charity/CharityRequestProvider";
+import { BusinessProvider } from "./businesses/BusinessProvider";
+import { DonationProvider } from "./donations/DonationProvider";
+import { BusinessTypeProvider } from "./businesses/BusinessTypeProvider";
+import { DonorProvider } from "./donations/DonorProvider";
 
 export default (props) => {
-    return (
-        <>
-                <CharityRequestProvider>
-                        <DonationProvider>
-                                <DonorProvider>
-                            <BusinessTypeProvider>
-                                <BusinessProvider>
-                            {props.children}
-                                </BusinessProvider>
-                            </BusinessTypeProvider>
-                                </DonorProvider>
-                        </DonationProvider>
-                </CharityRequestProvider>
-        </>
-    )
-}
+  return (
+    <>
+      <CharityRequestProvider>
+        <DonationProvider>
+          <DonorProvider>
+            <BusinessTypeProvider>
+              <BusinessProvider>
+                  {props.children}
+            </BusinessProvider>
+            </BusinessTypeProvider>
+          </DonorProvider>
+        </DonationProvider>
+      </CharityRequestProvider>
+    </>
+  );
+};
